5e9b13e7ea6c48c06fad870f56cbf77871858f8d
Suganya0411 <149463817+Suganya0411@users.noreply.github.com>
2023-11-06 10:28:20 +0530

    Update HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,7 +1,6 @@
-public class Hello {
+public class HelloJenkins{
 public static void main(String[] args) {
-	System.out.println("Hello World");
-	System.out.println("Hello Wipro");
+
 	System.out.println("Hello Jenkins");
 	
 	
